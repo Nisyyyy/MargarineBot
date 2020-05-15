56d7dcf9e2a0d4539d657fbca902d3e8c661c9dd
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2020-05-15 23:08:08 +0200

    Update kick.js

--- commands/Mod/kick.js
+++ commands/Mod/kick.js
@@ -1,19 +1,29 @@
 const { Command } = require("klasa");
-module.exports = {
-  name:"kick",
-  module:"moderation",
-  permission: ["KICK_MEMBERS"],
-  execute: (client, message, args) => {
-        var kickedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
-        if (!kickedmember) return message.reply("Please mention a valid user!"); // if there is no kickedmmeber var
-        if (!kickedmember.kickable) return message.reply("I cannot kick this member!") // if the member is unkickable
-        var kickreasondelete = 10 + kickedmember.user.id.length //sets the length of the kickreasondelete
-        var kickreason = message.content.substring(kickreasondelete).split(" "); // deletes the first letters until it reaches the reason
-        var kickreason = kickreason.join(" "); // joins the list kickreason into one line
-        if (!kickreason) return message.reply("Please indicate a reason for the kick!") // if no reason
-        kickedmember.kick(kickreason) //if reason, kick
-            .catch(error => message.reply(`Sorry @${message.author} I couldn't kick because of : ${error}`)); //if error, display error
-            message.reply(`${kickedmember.user.username} :tools: has been kicked by ${message.author.username} :tools: because: ${kickreason}`);
-  }
-}
-//hmm check discord,
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: "kick",
+            enabled: true,
+            runIn: ["text"],
+            aliases: ["k"],
+            permissionLevel: 5,
+            requiredPermissions: ["KICK_MEMBERS", "EMBED_LINKS"],
+            description: "Kicks the mentioned user.",
+            usage: "<user:usersearch> <reason:str>", usageDelim: ","
+        });
+    }
+
+    async run(msg, [user, reason]) {
+        user = msg.guild.members.cache.get(user.id);
+        if (user.kickable === false) { return msg.reply("I cannot kick that member"); }
+        
+        var data = this.client.util.modEmbed(msg, "kick", user, reason);
+        
+        if (data.embed.thumbnail) {
+            await user.send({embed: data.DMembed});
+            await user.kick(`Automated Action - Moderator: ${msg.author.username} | Reason: ${reason}`);
+        }
+
+        this.client.util.defaultChannel(msg.guild, "mod").send({embed: data.embed});
+    }
+};
