24f1cfe94551b145efebde312ca371d514cb85ba
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2020-05-15 23:17:28 +0200

    Update ban.js

--- commands/Mod/ban.js
+++ commands/Mod/ban.js
@@ -11,19 +11,19 @@ module.exports = class extends Command {
             description: "Ban someone.",
             usage: "<user:usersearch> <reason:str>", usageDelim: ","
         });
-    }
+    
 
-    async run(msg, [user, reason]) {
-        user = msg.guild.members.cache.get(user.id);
-        if (user.bannable === false) { return msg.reply("I cannot ban that member"); }
-        
-        var data = this.client.util.modEmbed(msg, "ban", user, reason);
-        
-        if (data.embed.thumbnail) {
-            await user.send({embed: data.DMembed});
-            await user.ban(`Automated Action - Moderator: ${msg.author.username} | Reason: ${reason}`);
-        }
-
-        this.client.util.defaultChannel(msg.guild, "mod").send({embed: data.embed});
+  execute: (client, message, args) => {
+    var bannedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
+  if (!bannedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
+  if (!bannedmember.kickable) return message.reply("I cannot ban this member!") // if the member is unkickable
+  var banreasondelete = 10 + bannedmember.user.id.length //sets the length of the kickreasondelete
+  var banreason = message.content.substring(banreasondelete).split(" "); // deletes the first letters until it reaches the reason
+  var banreason = banreason.join(" "); // joins the list kickreason into one line
+  if (!banreason) return message.reply("Please indicate a reason for the kick!") // if no reason
+  bannedmember.ban(banreason) //if reason, kick
+      .catch(error => message.reply(`Sorry @${message.author} I couldn't ban because of : ${error}`)); //if error, display error
+  message.channel.send(`${bannedmember.user.username} :hammer:  has been banned by ${message.author.username} :hammer:  because: ${banreason}`);
+  }
     }
 };
