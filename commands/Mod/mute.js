9fa81b58b266e37a72090ef4707a5bbb9f47fc11
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2020-05-16 15:50:21 +0200

    Update mute.js

--- commands/Mod/mute.js
+++ commands/Mod/mute.js
@@ -14,35 +14,89 @@ module.exports = class extends Command {
         });
     }
 
-    async run(msg, [user, reason="No reason given."]) {
-        if (msg.guild.settings.muteRole === null) { return msg.channel.send(this.client.speech(msg, ["mute", "noRole"])); }
-        user = msg.guild.members.cache.get(user.id);
+    	if (message.member.hasPermission("MANAGE_MESSAGES")) {
+		let member = message.mentions.members.first();
+		if(!member) {
+			var msgA = [];
+			msgA.push(`Correct Usage : **nmute [user] [time] [reason]** \n = **Muted Members:** =\n`)
 
-        var role = msg.guild.roles.cache.get(msg.guild.settings.muteRole);
-        if (role.position > msg.guild.members.cache.get(this.client.user.id).roles.highest.position) { return msg.channel.send(this.client.speech(msg, ["mute", "rolePos"])); }
+			message.guild.members.forEach(async (m) => {
+				if(m.roles.find("name", "Muted")) {
+					msgA.push(`${m.user.username} <${m.user.id}>`)
+				}
+			});
+			console.log(msgA)
+			if(msgA == `= **Muted Members** =\n`){
+					msgA.push(`== **Nobody is muted at the moment**==`)
+			}
+			message.channel.send(msgA).then(m => m.delete(25000));
+			return;
+		}
+		let arg = message.content.split(" ").slice(1);
+		let time = arg[1];
+		let rarg = message.content.split(" ").slice(3);
+		let reason = rarg.join(" ");
+		let id = member.user.id;
+		let musername = member.user.username;
+		if(!reason){reason = "No reason given."}
+		if(!time) {time = 0}
+		let muteRole = message.guild.roles.find("name", "Muted");
+		if(!muteRole){
+			message.channel.send("I couldn't find any role called `Muted`, I will create one! ")
+			muteRole = message.guild.createRole({"name": "Muted", "hoist": false, "position":0, "permissions": 0})
+			message.guild.channels.forEach(async (channel, i) => {
+				channel.overwritePermissions(muteRole.id, {SEND_MESSAGES: false, TALK: false}, "Had to make this for the mute role!")
+			});
+		}
 
-        if (msg.channel.permissionsFor(user).has("ADMINISTRATOR")) { //Admins can talk regardless. Send message to warn that it will not work.
-            msg.channel.send(this.client.speech(msg, ["mute", "admin"]));
-        }
+		if(member.roles.find("name", "Muted")) return message.channel.send(`:x: ${member.user.username}, is already muted!`).then(m => m.delete(2500))
+		 if(time == 0) {
+			 member.addRole(muteRole.id);
+			 let mutedrich = new Discord.RichEmbed()
+			 .setDescription(`✔️ **${member.user.username}**, is now muted.`)
+			 .setColor("RANDOM")
+	 		message.channel.send(mutedrich).then(m => m.delete(10000))
+	 		let muteds = new Discord.RichEmbed()
+	         .setColor(`#FF0000`)
+	         .setAuthor(`Hi, ${member.user.username}!`)
+	 				 .setDescription(`:mute: You are now muted in ${message.guild.name}.`)
+	         .addField(`Muted by:`, `${message.author.username}#${message.author.discriminator}`)
+	         .setFooter(`MUTED`)
+	         .setTimestamp()
+	 	 member.user.send({embed: muteds});
+		 return;
+		 }
+		member.addRole(muteRole.id);
+		let mutedrichc = new Discord.RichEmbed()
+		.setDescription(`**${member.user}**, is now muted for ${reason},  ${ms(ms(time), {long: true})}`)
+		.setColor("RANDOM")
+		message.channel.send(mutedrichc).then(m => m.delete(10000))
+		let muteds = new Discord.RichEmbed()
+        .setColor(`#FF0000`)
+        .setAuthor(`Hi, ${member.user.username}!`)
+				.setDescription(`:mute: You are now muted in ${message.guild.name}.`)
+      	.addField(`Time:`, `${ms(ms(time), {long: true})}`)
+        .addField(`Muted by:`, `${message.author.username}#${message.author.discriminator}`)
+        .addField(`Reason:`, `${reason}`)
+        .setFooter(`MUTED`)
+        .setTimestamp()
+	 member.user.send({embed: muteds}).then(m => m.delete(ms(time)))
+		setTimeout(function() {
 
-        if (user.roles.cache.get(role.id)) { 
-            var data = this.client.util.modEmbed(msg, "unmute", user, reason);
-            user.roles.remove(role.id, `Automated Action - Moderator: ${msg.author.username} | ${reason}`);
+			let mutede = new Discord.RichEmbed()
+	        .setColor(`#00FF00`)
+	        .setAuthor(`Hi, ${member.user.username}!`)
+					.setDescription(`:speaker: You are now unmuted in ${message.guild.name}.`)
+	      	.addField(`Time:`, `${ms(ms(time), {long: true})}`)
+	        .addField(`Reason:`, `${reason}`)
+	        .setFooter(`UNMUTED`)
+	        .setTimestamp()
+		  member.user.send({embed: mutede});
+			member.removeRole(muteRole.id);
+			message.channel.send(`${member.user.username}, is now unmuted from the last ${ms(ms(time), {long: true})}`).then(m => m.delete(2500))
+		}, ms(time))
+ } else {
+ 	message.channel.send("Sorry, but you don't have the required permissions.")
+ }
 
-            //Send embeds to logs and target user.
-            user.send({embed: data.DMembed});
-            this.client.util.defaultChannel(msg.guild, "mod").send({embed: data.embed});
-
-            return msg.channel.send(this.client.speech(msg, ["mute", "unmuted"], [["-user", user.user.username]]));
-        }
-
-        var data = this.client.util.modEmbed(msg, "mute", user, reason);
-        user.roles.add(role.id, `Automated Action - Moderator: ${msg.author.username} | ${reason}`);
-
-        //Send embeds to logs and target user.
-        user.send({embed: data.DMembed});
-        this.client.util.defaultChannel(msg.guild, "mod").send({embed: data.embed});
-        
-        msg.channel.send(this.client.speech(msg, ["mute", "muted"], [["-user", user.user.username]]));
-    }
 };
