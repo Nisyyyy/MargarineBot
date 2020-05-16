e44d95fb617bae5e7b2d4c55fab1d2f2f31176c6
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2020-05-16 15:12:15 +0200

    Update about.js

--- commands/System/about.js
+++ commands/System/about.js
@@ -17,15 +17,14 @@ module.exports = class extends Command {
         var support = (serverLink && serverLink.length > 1) ? `| [Support Server](${serverLink})` : "";
 
         const embed = new MessageEmbed()
-        .setColor(0x37FDFC)
+        .setColor('ORANGE')
         .setTitle("About Me")
-        .setDescription(`[Github](https://github.com/Butterstroke/MargarineBot) | [Terms Of Service](https://github.com/Butterstroke/MargarineBot/blob/master/TermsOfService.md) ${support}
-        \nI am a very helpful and amazing bot! Doing ${msg.guild.settings.prefix}help is great for finding ways I can assist you. I was written with Discord.js and Klasa, a Discord.js framework.
+        .setDescription(`[Github](https://github.com/Nisyyyy)
+        \nI am a very helpful and amazing bot! Doing ${msg.guild.settings.prefix}help
         \n**Stats:** I have been online, helping out, for ${this.client.util.timekeeper.elapsedTime(this.client.uptime)} using ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB of memory. ${this.client.users.cache.size} users across ${this.client.guilds.cache.size} guilds with ${this.client.channels.cache.size.toLocaleString()} channels depend on my functions to be as reliable as possible!
-        \n**Name Origin:** Butterstroke#7150's typical nickname is Butter. As in the stuff that you put on toast. My name comes from the artificial butter *(He tends to call it 'Fake Butter')* you can buy in stores called, Margarine.        
-        \n**Creation:** I was created on ${this.client.util.timekeeper.dateMaker(this.client.user.createdAt)} by Butterstroke#7150.`)
+        \n**Creation:** I was created on ${this.client.util.timekeeper.dateMaker(this.client.user.createdAt)} by Mario`)
         .setThumbnail(this.client.user.displayAvatarURL())
-        .setFooter(`Running on Margarine ${this.client.ownerSetting.get("build").version} | Released on: ${this.client.ownerSetting.get("build").releaseDate}`);
+        .setFooter(`Running on Heroku ${this.client.ownerSetting.get("build").version} | Released on: ${this.client.ownerSetting.get("build").releaseDate}`);
 
         msg.channel.send({embed});
     }
