be29c4a5817979e729ba5fb3948a6ec708e318f0
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2020-05-16 15:56:52 +0200

    Update presenceHelper.js

--- utilities/presenceHelper.js
+++ utilities/presenceHelper.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require("./discord.js");
 let games = require("../assets/localization.json")["games"];
 
 function Presence(client, type, name, status) {
