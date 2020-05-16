c21727be3c26d3eb50263430202f18af68732d7b
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2020-05-16 15:29:35 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -18,7 +18,7 @@ module.exports = class extends Event {
             .setTitle(`Greetings to the users of ${guild.name}!`)
             .setDescription(`My name is ${this.client.user.username} and I'd like to thank you for inviting me. I am a helpful and fantastic bot created with Klasa, a discord.js framework.\n
             If you want so see my awesome commands, feel free to do ${guild.settings.get("prefix")}help. I will send you my help menu where you will be provided with more information.\n
-            If you found something not quite right or have a suggestion, please use my report command and let my creator know! I do have a TOS for some of my abilities so please refer to [this link](https://github.com/Butterstroke/MargarineBot/blob/master/TermsOfService.md) for more information. I look forward to having fun with you!`);
+            If you found something not quite right or have a suggestion, please use my report command and let my creator know! I do have a TOS for some of my abilities so please refer to [this link](https://github.com/Nisyyyy) for more information. I look forward to having fun with you!`);
                 
         var channel = this.client.util.defaultChannel(guild);
         await channel.send({ embed });
